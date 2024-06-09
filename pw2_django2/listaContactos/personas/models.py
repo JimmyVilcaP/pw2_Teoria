8646a1375fcb334a806907570cfe21902dc03cd9
Jimmy Vilca Peralta <jvilcapera@unsa.edu.pe>
2024-06-08 20:58:17 -0500

    Se agrego tipos de datos apropiados para cada campo de Persona. Además se restringió la edad para un rango entre 0 y 100

--- pw2_django2/listaContactos/personas/models.py
+++ pw2_django2/listaContactos/personas/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Persona(models.Model):
-    nombres = models.TextField()
-    apellidos = models.TextField()
-    edad = models.TextField()
+    nombres = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=100)
+    edad = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
