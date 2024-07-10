2ea8495ba1d5d8e165089a0adf7f8c669151f54b
Jimmy Vilca Peralta <jvilcapera@unsa.edu.pe>
2024-07-09 21:51:39 -0500

    Porbando la creacion de componentes

--- angular/proyectoAngular/myappang/src/app/app.component.ts
+++ angular/proyectoAngular/myappang/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgIf, NgFor } from '@angular/common';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, NgFor],
+  imports: [RouterOutlet, NgIf, NgFor, HelloWorldComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'myappang';
   name: string;
