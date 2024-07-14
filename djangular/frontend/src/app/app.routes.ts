ff5287270de4be11f92eaa0e37ff12e60daad87a
Jimmy Vilca Peralta <jvilcapera@unsa.edu.pe>
2024-07-13 21:18:28 -0500

    Configurando las rutas en routes.ts y routing

--- djangular/frontend/src/app/app.routes.ts
+++ djangular/frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ListaTareasComponent } from './lista-tareas/lista-tareas.component';
+import { CrearTareaComponent } from './crear-tarea/crear-tarea.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ListaTareasComponent },
+  { path: 'crear', component: CrearTareaComponent }
+];
